a1140964e7c13294aedc9aa2d53f700d434560de
Rithwik <pr.rithwik.pro@gmail.com>
2026-09-04 17:17:10 +0530

    bug fixes

--- src/llm_practice/main.py
+++ src/llm_practice/main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 
-from constants.models import MODEL_OLLAMA_QWEN3_4B, MODEL_OLLAMA_LLAMA3_2_3B
-# from experiments import basic_completion, streaming
-# from experiments.ollama import main as get_ollama_completion
-from ui.simple_query import main as simple_query_ui
-from ui.simple_chat import main as simple_chat_ui
-from ui.tools_chat import main as tools_chat_ui
+from llm_practice.constants.models import MODEL_OLLAMA_QWEN3_4B, MODEL_OLLAMA_LLAMA3_2_3B
+# from llm_practice.experiments import basic_completion, streaming
+# from llm_practice.experiments.ollama import main as get_ollama_completion
+from llm_practice.ui.simple_query import main as simple_query_ui
+from llm_practice.ui.simple_chat import main as simple_chat_ui
+from llm_practice.ui.tools_chat import main as tools_chat_ui
 
 load_dotenv()
 
